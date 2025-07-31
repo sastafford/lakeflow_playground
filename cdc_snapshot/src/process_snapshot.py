19a68e4c923a6d9269a291fe266591b62ff31148
Scott Stafford <scott.stafford@databricks.com>
2025-07-30 22:35:25 -0400

    Add silver table type 1 and type 2

--- cdc_snapshot/src/process_snapshot.py
+++ cdc_snapshot/src/process_snapshot.py
@@ -1,10 +1,18 @@
 import dlt
 
-dlt.create_streaming_table("silver_person")
+dlt.create_streaming_table("silver_person_type_1")
+dlt.create_streaming_table("silver_person_type_2")
 
 dlt.create_auto_cdc_from_snapshot_flow(
- target="silver_person",
+ target="silver_person_type_2",
  source="person_snapshot",
  keys=["id"],
  stored_as_scd_type=2
+)
+
+dlt.create_auto_cdc_from_snapshot_flow(
+ target="silver_person_type_1",
+ source="person_snapshot",
+ keys=["id"],
+ stored_as_scd_type=1
 )
